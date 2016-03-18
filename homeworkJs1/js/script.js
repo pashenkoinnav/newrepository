3199ce0acbbd377e38e60e16d8dba82912fe7cfd
pashenkoinnav <pashenkoinnav@gmail.com>
2016-03-19 00:26:17 +0200

    Update script.js

--- homeworkJs1/js/script.js
+++ homeworkJs1/js/script.js
@@ -25,21 +25,3 @@ else {
 alert('Функция работает только с целыми числами, введите целое число');}
 
 
-var arr = [];
-for (var i = 0; i < 5; i++){
-    arr.push(prompt('Enter Name'));
-};
-
-var flag;
-var userName = prompt('Enter your User Name');
-for (i = 0; i < arr.length; i++){
-    if (arr[i] === userName){
-        flag = true;
-    }
-};
-if (flag == true) {
-    alert('Welcome, '+ userName );
-} else
-    {
-        alert('Sorry, but username' + userName + ' not found :(((')
-    };
