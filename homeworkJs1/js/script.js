b83f6b8e7990027d2cca079b631fb5415de7b625
pashenkoinnav <pashenkoinnav@gmail.com>
2016-03-19 10:29:24 +0200

    Update script.js
    aded else b<0

--- homeworkJs1/js/script.js
+++ homeworkJs1/js/script.js
@@ -1,21 +1,21 @@
 function pow(a, b) {
 	var result = a;
-	if (b==0) { result = 1;
+	if (b==0) { result = 1;	}
 
-	}
-
-
-	for (var i = 1; i < b; i++) {
-		result *=a;
+if (b<0)
+	{
+	for (var i = -1; i > b; i--)	{result *=a;}
 }
-
+else {
+	for (var i = 1; i < b; i++) {	result *=a;}
+}
 	return result;
 }
 var a = prompt("Введите число", '');
 var b = prompt("Введите степень",'');
 
 
-if (b-Math.floor(b)==0 && a-Math.floor(a)==0 ) {if (b<0) {console.log(1/pow(a, b));}
+if (b-Math.floor(b)==0 && a-Math.floor(a)==0 ) {if (b<0) {console.log(1/(pow(a, b)));}
 else {
 
 console.log(pow(a, b));
