05a346be91527bf0d8dc89dc5bc29fa67b386c46
pashenkoinnav <pashenkoinnav@gmail.com>
2016-04-04 15:17:53 +0300

    add homeworkJs6
    aded homeworkJs5-6

---
+++ homeworkJs6/js/script.js
@@ -0,0 +1,85 @@
+function myStart(){
+	var buttonName = this.innerHTML;
+	if (buttonName === 'PAUSE') {
+    this.innerHTML = 'CONTINUE';
+    clearInterval(intervalID);
+  } else {
+    this.innerHTML = 'PAUSE';
+	  intervalID = setInterval(startTimer,interval);
+	  startDate = new Date();
+  }
+}
+
+function myClear(){
+	buttonStart[0].innerHTML = 'START';
+	time.clear();
+	timer[0].innerText = time.toString();
+  	mills[0].innerHTML = time.showMillisec();
+  	clearInterval(intervalID);
+}
+
+function startTimer(){
+	var cur = new Date();
+	time.incMillisec(cur.getTime() - startDate.getTime());
+    startDate = cur;
+	timer[0].innerText = time.toString();
+	mills[0].innerHTML = time.showMillisec();
+}
+
+
+var time = {
+	hour: 0,
+	min: 0,
+	sec: 0,
+	millisec: 0,
+
+	clear : function() {
+		this.hour = 0;
+		this.min = 0;
+		this.sec = 0;
+		this.millisec = 0;
+	},
+	incSec : function() {
+		this.sec++;
+		if (this.sec == 60){
+			this.sec = 0;
+			this.min++;
+			if (this.min==60){
+				this.min = 0;
+				this.hour++;
+	}}
+	},
+  incMillisec : function(inter) {
+    this.millisec+=inter;
+    if(this.millisec >= 1000){
+      this.incSec();
+      this.millisec-=1000;
+    }
+  },
+	toString: function() {
+		var strHour = (this.hour > 9)? this.hour: '0' + this.hour;
+    var strMin = (this.min > 9)? this.min:'0'+ this.min;
+    var strSec = (this.sec > 9)? this.sec:'0' + this.sec;
+    return strHour + ':' + strMin + ':' + strSec;
+	},
+  showMillisec: function() {
+    if(this.millisec > 99){
+      return this.millisec+'';
+    }
+    if(this.millisec > 9){
+      return '0' + this.millisec;
+    }
+    return '00' + this.millisec;
+  }
+}
+var intervalID;
+var interval = 19;
+var startDate;
+var buttonStart = document.getElementsByClassName('start');
+buttonStart[0].addEventListener("click", myStart);
+
+var buttonClear = document.getElementsByClassName('clear');
+buttonClear[0].addEventListener("click", myClear);
+
+var timer = document.getElementsByClassName('timer');
+var mills = document.getElementsByClassName('millisec');
